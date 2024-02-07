4a08ad82df6125ca46c2039c7ac8a07aa7bc4d3f
Grimace of Despair <GrimaceOfDespair@hotmail.com>
2024-02-07 08:36:14 +0100

    Backward > forward slashes

--- Hackathon2024.Tests/TestTemplateRendering.cs
+++ Hackathon2024.Tests/TestTemplateRendering.cs
@@ -11,8 +11,8 @@
             var templateRender = new TemplateRenderer();
             templateRender.Render();
 
-            var givenExample = File.ReadAllText(@"..\..\..\..\Hackathon2024\result_template.html").Replace("\n", "").Replace("\r", "");
-            var result = File.ReadAllText(@"..\..\..\..\Hackathon2024\bin\Debug\net8.0\result.html").Replace("\n", "").Replace("\r", "");
+            var givenExample = File.ReadAllText(@"../../../../Hackathon2024/result_template.html").Replace("\n", "").Replace("\r", "");
+            var result = File.ReadAllText(@"../../../../Hackathon2024/bin/Debug/net8.0/result.html").Replace("\n", "").Replace("\r", "");
 
             var sequence1 = givenExample.Trim();
             var sequence2 = result.Trim();
